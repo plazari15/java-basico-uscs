1ffaf5f1e45997606e8551b3c95160203851b343
Pedro <plazari96@gmail.com>
2016-08-23 21:38:42 -0300

    Criando uma class que faz o While entre todas as letras do Alfabeto de A a Z

---
+++ javaBasico/src/WhileDemo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Plazari
+ */
+public class WhileDemo {
+    public static void main(String args[]){
+        char ch;
+        
+        //print the alphabet using a while loop 
+        ch = 'a';
+        while(ch <= 'z'){
+            System.out.print(ch);
+            ch++;
+        }
+    }
+}
