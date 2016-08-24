a64b2b7bc9bdf41bc3c625df39d5102dfd735f83
Pedro <plazari96@gmail.com>
2016-08-23 22:03:39 -0300

    Classe para Hipotenusa

---
+++ javaBasico/src/Hypot.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Plazari
+ */
+public class Hypot {
+    public static void main(String args[]){
+        double x,y,z;
+        
+        x=3;
+        y=4;
+        
+        z=Math.sqrt(x*x + y*y);
+        
+        System.out.println("A hipotenusa é"+z);
+    }
+}
--- javaBasico/src/SwithDemo.java
+++ javaBasico/src/SwithDemo.java
@@ -9,34 +9,35 @@
  * @author Plazari
  */
 public class SwithDemo {
-    public static void main (String args[]){
+
+    public static void main(String args[]) {
         int i;
-        for(i=0; i<10; i++)
-            switch(i){
+        for (i = 0; i < 10; i++) {
+            switch (i) {
                 case 0:
-                    System.out.println(i+"é zero");
+                    System.out.println(i + "é zero");
                     break;
-                    
+
                 case 1:
-                    System.out.println(i+" é one");
+                    System.out.println(i + " é one");
                     break;
-                    
+
                 case 2:
-                    System.out.println(i+" é two");
+                    System.out.println(i + " é two");
                     break;
-                    
-                    
+
                 case 3:
-                    System.out.println(i+" é Three");
+                    System.out.println(i + " é Three");
                     break;
-                    
+
                 case 4:
-                    System.out.println(i+" é Four");
+                    System.out.println(i + " é Four");
                     break;
-                    
+
                 default:
-                    System.out.println(i+" é o maior valor de I");
+                    System.out.println(i + " é o maior valor de I");
                     break;
             }
+        }
     }
 }
