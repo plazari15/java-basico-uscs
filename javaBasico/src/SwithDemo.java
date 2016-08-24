9ab1db97719361be19a2c0198882eefa3ee42dba
Pedro <plazari96@gmail.com>
2016-08-23 21:54:02 -0300

    Switch demo

---
+++ javaBasico/src/SwithDemo.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Plazari
+ */
+public class SwithDemo {
+    public static void main (String args[]){
+        int i;
+        for(i=0; i<10; i++)
+            switch(i){
+                case 0:
+                    System.out.println(i+"é zero");
+                    break;
+                    
+                case 1:
+                    System.out.println(i+" é one");
+                    break;
+                    
+                case 2:
+                    System.out.println(i+" é two");
+                    break;
+                    
+                    
+                case 3:
+                    System.out.println(i+" é Three");
+                    break;
+                    
+                case 4:
+                    System.out.println(i+" é Four");
+                    break;
+                    
+                default:
+                    System.out.println(i+" é o maior valor de I");
+                    break;
+            }
+    }
+}
